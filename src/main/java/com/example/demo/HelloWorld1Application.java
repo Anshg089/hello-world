c27b737e0f7093077cc15092e5b6aee8d17b8c2c
Anshg089 <131691091+Anshg089@users.noreply.github.com>
2023-04-27 10:01:41 +0530

    Update HelloWorld1Application.java

--- src/main/java/com/example/demo/HelloWorld1Application.java
+++ src/main/java/com/example/demo/HelloWorld1Application.java
@@ -23,7 +23,7 @@ public class HelloWorld1Application extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "hello Ansh Gupta";
+        return "Hello , I am Ansh Gupta";
     }
 
 }
